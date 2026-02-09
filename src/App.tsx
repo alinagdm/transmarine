2adc11f27e8be20ad76fa06036953e1e208951fb
alinagdm <141248355+alinagdm@users.noreply.github.com>
2026-02-10 01:43:20 +0200

    Добавлена автоматическая прокрутка вверх при переходе на другую страницу

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
@@ -85,11 +85,22 @@ function HomePage() {
   );
 }
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [pathname]);
+
+  return null;
+}
+
 function AppContent() {
   const { isOpen, closeForm } = usePortCalculation();
 
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
